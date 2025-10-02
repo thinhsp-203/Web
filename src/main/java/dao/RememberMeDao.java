dd27fc5c24605ecc3bdee14750280d144aea7a53
thinhsp-203 <jknguyen522@gmail.com>
2025-10-02 08:38:52 +0700

    Create RememberMeDao.java

---
+++ src/main/java/dao/RememberMeDao.java
@@ -0,0 +1,10 @@
+package dao;
+
+import model.RememberMeToken;
+
+public interface RememberMeDao {
+    void save(RememberMeToken t) throws Exception;
+    RememberMeToken findBySelector(String selector) throws Exception;
+    void deleteBySelector(String selector) throws Exception;
+    void deleteAllForUser(long userId) throws Exception;
+}
